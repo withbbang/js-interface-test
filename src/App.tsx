310f7fc6fd7e77fef84d5c5fd0cfbdb6c2ced8e9
김영선 <withbbang@gmail.com>
2023-06-24 21:51:56 +0900

    react 자바스크립트 인터페이스 테스트용 프로젝트

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (): JSX.Element => {
+  const [toastMessageFromNative, setToastMessageFromNative] =
+    useState<string>('');
+
+  const handleEventFromNative = async (e: any) => {
+    setToastMessageFromNative(e.data);
+  };
+
+  useEffect(() => {
+    window.addEventListener('calledFunctionByNative', handleEventFromNative);
+
+    if (window.callFunctionByWeb) {
+      window.callFunctionByWeb.showToastMessage('나는 K상남자~!');
+    }
+  }, []);
+
+  return <>Sex!</>;
+};
 
 export default App;
