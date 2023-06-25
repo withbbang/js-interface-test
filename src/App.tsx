e18e469e1d9097762a4d31c424101e692851df4f
김영선 <withbbang@gmail.com>
2023-06-25 09:54:41 +0900

    web -> native 함수 호출 버튼 생성

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,21 @@ const App = (): JSX.Element => {
   useEffect(() => {
     // Native -> Web
     window.addEventListener('calledFunctionByNative', handleEventFromNative);
+  }, []);
 
+  const handleCallNativeFunction = () => {
     // Web -> Native
     if (window.calledFunctionByWeb) {
       window.calledFunctionByWeb.showToastMessage('나는 K상남자~!');
     }
-  }, []);
+  };
 
-  return <h1>Javascript Interface Test</h1>;
+  return (
+    <div>
+      <h1>Javascript Interface Test</h1>
+      <button onClick={handleCallNativeFunction}>Call Native Function</button>
+    </div>
+  );
 };
 
 export default App;
