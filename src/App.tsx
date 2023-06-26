a4bf96b1745f3b5dcc4b07a4f242a396cf634810
김영선 <withbbang@gmail.com>
2023-06-26 16:36:34 +0900

    무조건 앱에 트리거 주업

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,37 @@ import { useEffect } from 'react';
 const App = (): JSX.Element => {
   useEffect(() => {
     // Native -> Web
-    window.addEventListener('calledFunctionByNative', handleEventFromNative);
+    window.addEventListener('showAlert', handleEventFromNative);
   }, []);
 
   const handleEventFromNative = async (data: any) => {
     alert(data);
+    console.log(data);
   };
 
-  const handleCallNativeFunction = () => {
+  const handleShowToastMessage = () => {
     // Web -> Native
-    if (window.calledFunctionByWeb) {
-      window.calledFunctionByWeb.showToastMessage('나는 K상남자~!');
+    if (window.android) {
+      window.android.showToastMessage('나는 K상남자~!');
+    }
+  };
+
+  const handleShowAlertMessage = () => {
+    if (window.android) {
+      window.android.showAlertMessage();
     }
   };
 
   const reactFunc = (data: any) => {
-    alert('data in reactFunc:, ' + data);
+    alert('data in reactFunc: ' + data);
+    console.log(data);
   };
 
   return (
     <div>
       <h1>Javascript Interface Test</h1>
-      <button onClick={handleCallNativeFunction}>Call Native Function</button>
+      <button onClick={handleShowToastMessage}>showToastMessage</button>
+      <button onClick={handleShowAlertMessage}>showAlertMessage</button>
     </div>
   );
 };
