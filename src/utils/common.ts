a9256ce698a5af46aeed35b3b20245df3929029d
김영선 <withbbang@gmail.com>
2023-06-29 18:15:07 +0900

    js 양방향 한큐에 테스트

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,21 +1,41 @@
 /**
+ * 화면 접근 OS 반환 함수
+ * @returns {string}
+ */
+export const handleGetOsType = (): string => {
+  const { userAgent } = navigator;
+
+  if (/android/i.test(userAgent)) {
+    return 'AND';
+  } else if (/iPad|iPhone|iPod/.test(userAgent)) {
+    return 'IOS';
+  } else {
+    return 'BACK';
+  }
+};
+
+/**
  * Android용 Javascript Interface
- * @param {string} action 액션이름
- * @param {any} data 콜백 데이터
+ * @param {string} action 액션 이름
+ * @param {any | undefined} data 콜백 데이터
+ * @param {string | undefined} bridge 브릿지 이름
  * @returns {Promise<any>}
  */
 export const handleJavascriptInterface = (
   action: string,
-  data: any
+  data?: any,
+  bridge?: string
 ): Promise<string> => {
   return new Promise((resolve, reject) => {
-    if (window.android) {
+    const interfaceNm = (bridge || 'android') as keyof Window;
+
+    if (window[interfaceNm]) {
       window.addEventListener(action, (e: any) => {
         resolve(e.detail.data);
       });
 
-      if (window.android[action]) {
-        window.android[action](data);
+      if (window[interfaceNm][action]) {
+        window[interfaceNm][action](data);
       } else {
         reject();
       }
