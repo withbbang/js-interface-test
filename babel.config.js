35cb14f143651c7547d164907dfbf80fce556418
김영선 <withbbang@gmail.com>
2023-06-29 13:45:30 +0900

    babel 콘솔 지우기 테스트

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins:
     process.env.REACT_APP_MODE === 'production'
-      ? ['transform-remove-console', { exclude: ['error', 'warn'] }]
+      ? [['transform-remove-console', { exclude: ['error', 'warn'] }]]
       : []
 };
